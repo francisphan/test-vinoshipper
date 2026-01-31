40a50fdcb4603c3c9c8b0b611fca87fc0953e744
Francis Phan <fs.phan@gmail.com>
2026-01-31 15:00:02 -0300

    Fix bad import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import VinoshipperAgent from '../multi-account-proto';
+import VinoshipperAgent from './VinoshipperAgent';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
